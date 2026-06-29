753d00837b0cffd98c41cd9480f334eeb38a683f
renan veezor <renan.balbino56@gmail.com>
2026-06-28 22:05:24 -0300

    feat: tela de registro de paciente com campos dinâmicos por espécie

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout'
 import RotaProtegida from './components/RotaProtegida'
 import Login from './pages/Login'
 import RegistrarMedico from './pages/RegistrarMedico'
+import RegistrarPaciente from './pages/RegistrarPaciente'
 import Home from './pages/Home'
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/registrar" element={<RegistrarMedico />} />
+      <Route path="/registrar-paciente" element={<RegistrarPaciente />} />
       <Route
         element={
           <RotaProtegida>
